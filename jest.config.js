822c0df99003272dce823ee590bc067f3e13d76e
brunenrique <93277400+brunenrique@users.noreply.github.com>
2025-06-19 21:52:32 -0300

    Add files via upload

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+};
---
+++ next.config.ts
@@ -0,0 +1,29 @@
+
+import type { NextConfig } from 'next';
+
+/** @type {import('next').NextConfig} */
+const nextConfig: NextConfig = {
+  // typescript: {
+  //   ignoreBuildErrors: true,
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+
+  allowedDevOrigins: [
+    'https://9003-firebase-studio-1749490048431.cluster-duylic2g3fbzerqpzxxbw6helm.cloudworkstations.dev',
+    'https://3000-firebase-studio-1749490048431.cluster-duylic2g3fbzerqpzxxbw6helm.cloudworkstations.dev',
+  ],
+};
+
+export default nextConfig;
